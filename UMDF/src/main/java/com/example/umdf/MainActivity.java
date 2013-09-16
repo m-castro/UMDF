7aed33a1a01c1dd1c3dd9cdd226f0ce344266a64
m-castro <castro96@yahoo.com>
2013-09-16 13:50:54 -0500

    set basic layouts

--- UMDF/src/main/java/com/example/umdf/MainActivity.java
+++ UMDF/src/main/java/com/example/umdf/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.umdf;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +13,46 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button infoButton = (Button) findViewById(R.id.information);
+        Button connectButton = (Button) findViewById(R.id.connect);
+        Button donateButton = (Button) findViewById(R.id.donate);
+        Button contactButton = (Button) findViewById(R.id.contact);
+
+        infoButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.info_layout);
+//                Intent infoIntent = new Intent(setContentView(R.layout.info_layout));
+            }
+        });
+
+        connectButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.connect_layout);
+            }
+        });
+
+        donateButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.donate_layout);
+            }
+        });
+
+        contactButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.contact_layout);
+            }
+        });
+
+
     }
 
 
