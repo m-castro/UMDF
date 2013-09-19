9c12d57e75aa875560ac8ef0aa5179ce0fb5dac3
m-castro <castro96@yahoo.com>
2013-09-19 18:06:21 -0500

    changed connect activity file added social media icons

--- UMDF/src/main/java/com/example/umdf/Connect.java
+++ UMDF/src/main/java/com/example/umdf/Connect.java
@@ -4,19 +4,30 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.TextView;
 
 /**
  * Created by Manny on 9/16/13.
  */
 public class Connect extends Activity {
 
+    TextView mTextView;
+    String socialMedia;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       setContentView(R.layout.connect_layout);
+        setContentView(R.layout.connect_layout);
 
-        //Uri uri = Uri.parse("https://www.facebook.com/pages/United-Mitochondrial-Disease-Foundation/202570833099467");
-        //Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-        //startActivity(intent);
+        mTextView = (TextView)findViewById(R.id.socialMediaTextView);
+
+        socialMedia = "Selecting any of the icons below\n" +
+                "will launch that particular social media site for the UMDF";
+
+        mTextView.setText(socialMedia);
+
+//        Uri uri = Uri.parse("https://www.facebook.com/pages/United-Mitochondrial-Disease-Foundation/202570833099467");
+//        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+//        startActivity(intent);
     }
 }
