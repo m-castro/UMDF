03d9a118549b7377f06a594c8e7e9eff7e975252
David Housman <david.nyttsheet@gmail.com>
2013-09-20 11:01:44 -0500

    updated button and icons part 6

--- UMDF/src/main/java/com/example/umdf/Connect.java
+++ UMDF/src/main/java/com/example/umdf/Connect.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 /**
@@ -19,6 +21,11 @@ public class Connect extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.connect_layout);
 
+        ImageView Facebook = (ImageView) findViewById(R.id.facebookIcon);
+        ImageView Twitter = (ImageView) findViewById(R.id.twitterIcon);
+        ImageView GooglePlus = (ImageView) findViewById(R.id.googlePlusIcon);
+        ImageView YouTube = (ImageView) findViewById(R.id.youTubeIcon);
+
         mTextView = (TextView)findViewById(R.id.socialMediaTextView);
 
         socialMedia = "Selecting any of the icons below\n" +
@@ -26,8 +33,44 @@ public class Connect extends Activity {
 
         mTextView.setText(socialMedia);
 
-//        Uri uri = Uri.parse("https://www.facebook.com/pages/United-Mitochondrial-Disease-Foundation/202570833099467");
-//        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-//        startActivity(intent);
+        Facebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://www.facebook.com/pages/United-Mitochondrial-Disease-Foundation/202570833099467");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        Twitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://twitter.com/UMDF");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        GooglePlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://plus.google.com/109599791610901846365/posts");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        YouTube.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("http://www.youtube.com/user/UMDF1");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
+
+
