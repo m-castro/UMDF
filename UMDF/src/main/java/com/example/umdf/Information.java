4fe424279af8ebac5371571a6ca7e0d43f1d9b27
Devian Simmons <devians80@gmail.com>
2013-09-21 22:38:39 -0500

    Added list view connection to web parse

--- UMDF/src/main/java/com/example/umdf/Information.java
+++ UMDF/src/main/java/com/example/umdf/Information.java
@@ -1,86 +1,47 @@
 package com.example.umdf;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 /**
  * Created by Manny on 9/16/13.
  */
-public class Information extends MainActivity {
-
-    private ListView listView;
-    private ArrayAdapter<String> listAdapter;
+public class Information extends Activity  {
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.info_layout);
-        String[] names = new String[]{
-                "Info"
 
-        };
+        final String [] str = {"What is Mitochondrial Disease","Types of Mitochondrial Disease","Possible Symptoms", "Getting a Diagnosis","Treatments & Therapies","FAQ's", "Other Diseases Linked to Mitochondrial Disease"};
+        final ListView lv = (ListView) findViewById(R.id.listView);
 
+        ArrayAdapter<Object> adapt = new ArrayAdapter<Object>(getApplicationContext(), android.R.layout.simple_list_item_1, str);
+        lv.setAdapter(adapt);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+//                super.onItemClick(l, v, position, id);
+//                String string = str[position];
 
-        this.setListAdapter(new ArrayAdapter<String>(this, R.layout.info_layout,names));
-//       ArrayAdapter<String>  = new ArrayAdapter<String>(getApplicationContext(),R.layout.info_layout);
-//        listView = (ListView) findViewById(R.id.listView);
+                try{
+                    Uri uri = Uri.parse("http://www.umdf.org/site/pp.aspx?c=8qKOJ0MvF7LUG&b=7934627");
+                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                    startActivity(intent);
+
+                }catch (Exception e){
+                    e.printStackTrace();
+            }
+            }
+
+        });
+
     }
-
-
-
-//        AdapterView listView = null;
-//        if (listView != null) {
-//
-//        listView.setAdapter(listAdapter);
-//        listView.setOnItemClickListener(new ListView.OnItemClickListener() {
-    @Override
-    public void onListItemClick(ListView l, View v, int position, long id) {
-
-        super.onListItemClick(l,v,position,id);
-        Intent in = new Intent(this,WebViewPage.class);
-        startActivity(in);
-    }
-
-//
-
-//                WebView wbView = (WebView) findViewById(R.id.WebView);
-//                wbView.getSettings().setJavaScriptEnabled(true);
-//                wbView.loadUrl("http://www.google.com");
-//    }
-//        try{
-//
-//                Uri uri = Uri.parse("https://secure.umdf.org/site/apps/ka/sd/donor.asp?c=8qKOJ0MvF7LUG&b=7966007&en=joLIIKNoH3IALNOpG4IBJOOnFeKVK0PyEkLLLPPrE7KDJPOzFsG");
-//                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-//                startActivity(intent);}
-
-//        }catch(Exception exception)
-//            {
-//                Toast.makeText(getApplicationContext(), "The link isn't working",Toast.LENGTH_LONG).show();
-//            }
-//
-//        }
-//
-//            @Override
-//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-//
-//            }
-
-    public void setListAdapter(ArrayAdapter<String> listAdapter) {
-        this.listAdapter = listAdapter;
-    }
-
-    //});
-//
-//    }
-//
-//
-//    }
 }
-
-
-
