973afd442bbdb46057900b2948a43c28676992e0
David Housman <david.nyttsheet@gmail.com>
2013-09-23 15:25:39 -0500

    update to buttons and the connect .xml & JAVA.

--- UMDF/src/main/java/com/example/umdf/Connect.java
+++ UMDF/src/main/java/com/example/umdf/Connect.java
@@ -13,64 +13,96 @@ import android.widget.TextView;
  */
 public class Connect extends Activity {
 
-    TextView mTextView;
-    String socialMedia;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.connect_layout);
-
-        ImageView Facebook = (ImageView) findViewById(R.id.facebookIcon);
-        ImageView Twitter = (ImageView) findViewById(R.id.twitterIcon);
-        ImageView GooglePlus = (ImageView) findViewById(R.id.googlePlusIcon);
-        ImageView YouTube = (ImageView) findViewById(R.id.youTubeIcon);
-
-        mTextView = (TextView)findViewById(R.id.socialMediaTextView);
-
-        socialMedia = "Select Icon Below To Launch:";
 
 
-        mTextView.setText(socialMedia);
+        TextView mTextView;
+        TextView mTextTwo;
+        TextView mTextThree;
+        String socialMedia;
+        String phone;
+        String email;
 
-        Facebook.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Uri uri = Uri.parse("https://www.facebook.com/pages/United-Mitochondrial-Disease-Foundation/202570833099467");
-                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(intent);
-            }
-        });
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.connect_layout);
 
-        Twitter.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Uri uri = Uri.parse("https://twitter.com/UMDF");
-                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(intent);
-            }
-        });
+            ImageView Facebook = (ImageView) findViewById(R.id.facebookIcon);
+            ImageView Twitter = (ImageView) findViewById(R.id.twitterIcon);
+            ImageView GooglePlus = (ImageView) findViewById(R.id.googlePlusIcon);
+            ImageView YouTube = (ImageView) findViewById(R.id.youTubeIcon);
+            ImageView Email = (ImageView) findViewById(R.id.emailIconTwo);
+            ImageView Phone = (ImageView) findViewById(R.id.phoneIconTwo);
 
-        GooglePlus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Uri uri = Uri.parse("https://plus.google.com/109599791610901846365/posts");
-                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(intent);
-            }
-        });
+            mTextView = (TextView)findViewById(R.id.socialMediaTextView);
+            socialMedia = "Select Icon Below To Launch:";
+            mTextView.setText(socialMedia);
 
-        YouTube.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Uri uri = Uri.parse("http://www.youtube.com/user/UMDF1");
-                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(intent);
-            }
-        });
+            mTextTwo = (TextView) findViewById(R.id.phoneNumber);
+            phone = "Call Us:";
+            mTextTwo.setText(phone);
+
+            mTextThree = (TextView) findViewById(R.id.email);
+            email = "Email Us:";
+            mTextThree.setText(email);
 
 
+            Facebook.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Uri uri = Uri.parse("https://www.facebook.com/pages/United-Mitochondrial-Disease-Foundation/202570833099467");
+                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                    startActivity(intent);
+                }
+            });
+
+            Twitter.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Uri uri = Uri.parse("https://twitter.com/UMDF");
+                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                    startActivity(intent);
+                }
+            });
+
+            GooglePlus.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Uri uri = Uri.parse("https://plus.google.com/109599791610901846365/posts");
+                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                    startActivity(intent);
+                }
+            });
+
+            YouTube.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Uri uri = Uri.parse("http://www.youtube.com/user/UMDF1");
+                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                    startActivity(intent);
+                }
+            });
+
+            Email.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Uri uri = Uri.parse("https://plus.google.com/109599791610901846365/posts");
+                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                    startActivity(intent);
+                }
+            });
+
+            Phone.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Uri uri = Uri.parse("https://plus.google.com/109599791610901846365/posts");
+                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                    startActivity(intent);
+                }
+            });
+
+
+        }
     }
-}
 
 
